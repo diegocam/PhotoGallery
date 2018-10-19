55bce551c7d513ed85e374d63cd30b7870e8b5f2
Diego Camacho <camachojcd@gmail.com>
2018-10-19 00:53:42 -0700

    Let PhotoGallery accept array of images

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class PhotoGallery extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            // images array: each image should have a "url" and "caption"
+            images: this.props.images
+        }
+    }
+
     render() {
         return (
             <div className="photo-gallery">
