1bd836478a441ab055b8e1a99d65f622e4012951
Diego Camacho <camachojcd@gmail.com>
2018-10-19 00:43:39 -0700

    Create PhotoGallery component

---
+++ src/components/PhotoGallery.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PhotoGallery extends Component {
+    render() {
+        return (
+            <div className="photo-gallery">
+
+            </div>
+        );
+    }
+}
+
+export default PhotoGallery;
