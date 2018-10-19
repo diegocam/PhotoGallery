9f1197dcf0aaf31ab6c3c442a952cf47eed6e4f7
Diego Camacho <camachojcd@gmail.com>
2018-10-19 00:55:16 -0700

    Create Photo component

---
+++ src/components/Photo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Photo extends Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Photo;
