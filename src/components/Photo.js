dd2587078a427cd62dc5450fdce3852b973efc78
Diego Camacho <camachojcd@gmail.com>
2018-10-19 01:04:15 -0700

    Allow Photo component to receive image and render it

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
 class Photo extends Component {
+    constructor() {
+        super();
+        this.state = {
+            image: this.props.image
+        }
+    }
 
     render() {
         return (
-            <div>
-
+            <div className="photo">
+                <img src={this.state.image.url}/>
+                <div className="caption">{this.state.image.caption}</div>
             </div>
         );
     }
