46659a8955d398856b5fd415bb762e4597c2bcfc
Diego Camacho <camachojcd@gmail.com>
2018-10-19 00:58:16 -0700

    Iterate over images and use Photo component per each image

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Photo from './Photo';
 
 class PhotoGallery extends Component {
 
@@ -13,7 +14,9 @@ class PhotoGallery extends Component {
     render() {
         return (
             <div className="photo-gallery">
-
+                {this.state.images.map((image) =>
+                    <Photo image={image}/>
+                )}
             </div>
         );
     }
