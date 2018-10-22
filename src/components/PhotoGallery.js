0b383af0d51c06d4217181018e92c76abc005ddd
Diego Camacho <camachojcd@gmail.com>
2018-10-22 11:43:12 -0700

    Add drag logic and slide on condition

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -14,33 +14,54 @@ class PhotoGallery extends Component {
         };
         this.sliding = false
         this.startClientX = 0
-        this.deltaSlide = 0
+        this.sliderX = 0
+        this.currImage = 1
     }
 
     onStart(e) {
         if (!this.sliding) {
             this.sliding = true
-            this.startClientX = e.clientX - this.deltaSlide
+            this.startClientX = e.clientX
+            this.startClientXDiff = e.clientX - this.sliderX
         }
     }
 
     onMove(e) {
         if (this.sliding) {
-            this.deltaSlide = e.clientX - this.startClientX
-            if (this.deltaSlide <= 0) {
-                e.target.style.transform = "translateX(" + this.deltaSlide + "px)"
-            }
+            this.distance = e.clientX - this.startClientX
+            this.currSlide = e.clientX - this.startClientXDiff
+            e.target.style.transform = "translateX(" + this.currSlide + "px)"
         }
     }
 
-    onEnd() {
-        this.sliding = false
+    onEnd(e) {
+        if (this.sliding) {
+
+            // allow change move distance if it is in range
+            if (Math.abs(this.distance) > 100) {
+                const sliderBounds = e.target.getBoundingClientRect();
+                const galleryBounds = e.target.parentElement.getBoundingClientRect();
+
+                // if left < 0, prevent drag
+                if (this.currSlide > 0 && galleryBounds.left < sliderBounds.left) {
+                    this.sliderX = 0
+                    // if right > slider.leng prevent drag
+                } else if (sliderBounds.right < galleryBounds.right) {
+                    this.sliderX = -(this.state.images.length * this.imgWidth) + this.imgWidth
+                } else {
+                    this.sliderX = this.sliderX - (this.distance > 0 ? -800 : 800)
+                }
+            }
+
+            e.target.style.transform = "translateX(" + this.sliderX + "px)"
+            this.sliding = false
+        }
     }
 
     render() {
         return (
             <div className="photo-gallery">
-                <div className="slider"
+                <div className={"slider " + (this.sliding ? "test" : "anothe")}
                     style={this.state.sliderStyle}
                     onMouseDown={(e) => this.onStart(e)}
                     onMouseMove={(e) => this.onMove(e)}
